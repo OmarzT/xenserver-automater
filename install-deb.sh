cbbed23779b59eceb0fc065bdef85856ee9fe5c0
Omar Tamer <omarzt@gmail.com>
2013-09-01 22:28:24 +0100

    Update install-deb.sh

--- install-deb.sh
+++ install-deb.sh
@@ -1,8 +1,8 @@
 # Copy scripts
 cp usr/sbin/xe-set-hostname /usr/sbin/xe-set-hostname
 cp usr/sbin/xe-set-network-deb /usr/sbin/xe-set-network
-cp etc/init.d/xe-automator.sh /etc/init.d/xe-automator.sh
+cp etc/init.d/xe-automater.sh /etc/init.d/xe-automater.sh
 chmod a+x /usr/sbin/xe-set-hostname
 chmod a+x /usr/sbin/xe-set-network
-chmod a+x /etc/init.d/xe-automator.sh 
-update-rc.d /etc/init.d/xe-automator.sh defaults
+chmod a+x /etc/init.d/xe-automater.sh 
+update-rc.d /etc/init.d/xe-automater.sh defaults
